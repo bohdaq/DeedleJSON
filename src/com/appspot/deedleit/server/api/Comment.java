5b5313447a8bfcd4a1101c1833a0b7ef8327e1cc
Bogdan Tsap <bohdaq@gmail.com>
2012-10-15 00:31:55 +0300

    comment GET impl, deedId parameter

--- src/com/appspot/deedleit/server/api/Comment.java
+++ src/com/appspot/deedleit/server/api/Comment.java
@@ -13,6 +13,13 @@ import com.appspot.deedleit.server.json.JSONUtil;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 import com.google.gson.Gson;
 
 @SuppressWarnings("serial")
@@ -29,11 +36,39 @@ public class Comment extends HttpServlet {
 		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
 		Entity comment = new Entity("comments");
 		comment.setProperty("email", commentJsonObj.getEmail());
-		comment.setProperty("photoId", commentJsonObj.getPhotoId());
+		comment.setProperty("deedId", commentJsonObj.getDeedId());
 		comment.setProperty("comment", commentJsonObj.getComment());
+		comment.setProperty("userPhotoId", commentJsonObj.getUserPhotoId());
 		ds.put(comment);
 		out.println(JSONUtil.success());
 	}
 
-	public static final String SEPARATOR = "#$*";
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+		String deedId = req.getParameter("deedId");
+		Filter deedFilter = new FilterPredicate("city", Query.FilterOperator.EQUAL, deedId);
+		Query query = new Query("comments");
+		query.setFilter(deedFilter);
+		PreparedQuery pq = ds.prepare(query);
+		JSONArray jArray = new JSONArray();
+		for (Entity e : pq.asIterable()) {
+			JSONObject commentJSON = new JSONObject();
+			try {
+				commentJSON.put("email", e.getProperty("email"));
+				commentJSON.put("deedId", e.getProperty("deedId"));
+				commentJSON.put("comment", e.getProperty("comment"));
+				commentJSON.put("userPhotoId", e.getProperty("userPhotoId"));
+			} catch (JSONException e1) {
+				e1.getMessage();
+			}
+			jArray.put(commentJSON);
+		}
+		String result = jArray.toString();
+		out.println(result);
+	}
+
+	//public static final String SEPARATOR = "#$*";
 }
--- src/com/appspot/deedleit/server/entity/CommentEntity.java
+++ src/com/appspot/deedleit/server/entity/CommentEntity.java
@@ -2,25 +2,32 @@ package com.appspot.deedleit.server.entity;
 
 public class CommentEntity {
 	private String email;
-	private String photoId;
+	private String deedId;
 	private String comment;
+	private String userPhotoId;
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getPhotoId() {
-		return photoId;
+	public String getDeedId() {
+		return deedId;
 	}
-	public void setPhotoId(String photoId) {
-		this.photoId = photoId;
+	public void setDeedId(String photoId) {
+		this.deedId = photoId;
 	}
 	public String getComment() {
 		return comment;
 	}
 	public void setComment(String comment) {
 		this.comment = comment;
+	}
+	public String getUserPhotoId() {
+		return userPhotoId;
+	}
+	public void setUserPhotoId(String userPhotoId) {
+		this.userPhotoId = userPhotoId;
 	}
 
 }
